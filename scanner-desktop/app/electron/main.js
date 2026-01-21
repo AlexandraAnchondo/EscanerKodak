37a8fb4a0645134cf2c7978b8378a4fab6dcb6a7
AlexandraAnchondo <alexandra.anchondo.17@gmail.com>
2026-01-21 11:34:26 -0800

    Enhance performance

--- scanner-desktop/app/electron/main.js
+++ scanner-desktop/app/electron/main.js
@@ -23,31 +23,41 @@ function createWindow() {
 }
 
 // ⏳ Espera a que un endpoint responda
-async function waitFor(url, name, retries = 20) {
-    for (let i = 0; i < retries; i++) {
+async function waitFor(url, name, retries = 30, delay = 1000) {
+    for (let i = 1; i <= retries; i++) {
         try {
             const res = await fetch(url)
             if (res.ok) {
                 console.log(`✅ ${name} listo`)
                 return
             }
-        // eslint-disable-next-line no-unused-vars
-        } catch (_) { /* empty */ }
+        } catch { /* empty */ }
 
-        console.log(`⏳ Esperando ${name}...`)
-        await new Promise(r => setTimeout(r, 500))
+        console.log(`⏳ ${name} intento ${i}/${retries}`)
+        await new Promise(r => setTimeout(r, delay))
     }
 
-    throw new Error(`❌ ${name} no respondió`)
+    throw new Error(`${name} no respondió`)
 }
+
 
 app.whenReady().then(async () => {
     // 🟩 Scanner .NET PRIMERO
     scannerProcess = spawn(
         join(__dirname, '../../scanner/scanner-service.exe'),
         [],
+        {
+            stdio: 'inherit',
+            windowsHide: true,
+            shell: true
+        },
         { stdio: 'inherit', windowsHide: true }
     )
+
+    scannerProcess.on('exit', (code) => {
+        console.error('❌ Scanner service salió con código', code)
+        app.quit()
+    })
 
     try {
         await waitFor('http://localhost:5000/health', 'Scanner Service', 40)
@@ -58,6 +68,11 @@ app.whenReady().then(async () => {
             [join(__dirname, '../../backend/server.js')],
             { stdio: 'inherit', windowsHide: true }
         )
+
+        backendProcess.on('exit', (code) => {
+            console.error('❌ Backend salió con código', code)
+            app.quit()
+        })
 
         await waitFor('http://localhost:3001/ping', 'Backend', 20)
 
